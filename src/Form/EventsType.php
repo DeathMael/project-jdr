9e1b930f170958de6b9f794a58285d648e530fed
Willy <willysterr@gmail.com>
2019-11-30 11:23:58 +0100

    event OK mais bugg calendar

--- src/Form/EventsType.php
+++ src/Form/EventsType.php
@@ -4,25 +4,26 @@ namespace App\Form;
 
 use App\Entity\Events;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class EventsType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('date')
-            ->add('hour')
-            ->add('description')
-            ->add('title')
-        ;
-    }
+class EventsType extends AbstractType {
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$builder
+			->add('date', DateType::class)
+			->add('hour', TimeType::class)
+			->add('description')
+			->add('title')
+			->add('start')
+			->add('end')
+		;
+	}
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => Events::class,
-        ]);
-    }
+	public function configureOptions(OptionsResolver $resolver) {
+		$resolver->setDefaults([
+			'data_class' => Events::class,
+		]);
+	}
 }
