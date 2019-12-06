6f3743344a08783480e4bd4d9664336e4a1290e7
Willy <willysterr@gmail.com>
2019-12-06 12:16:18 +0100

    date verif ok

--- src/Entity/Booking.php
+++ src/Entity/Booking.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BookingRepository")
@@ -19,16 +20,23 @@ class Booking {
 
 	/**
 	 * @ORM\Column(type="datetime")
+	 * @Assert\NotBlank
+	 * @Assert\GreaterThan("today Europe/Paris")
 	 */
 	private $beginAt;
 
 	/**
 	 * @ORM\Column(type="datetime", nullable=true)
+	 * @Assert\Expression(
+	 *     "this.getBeginAt() < this.getEndAt()",
+	 *     message="La date fin ne doit pas être antérieure à la date début"
+	 * )
 	 */
 	private $endAt = null;
 
 	/**
 	 * @ORM\Column(type="string", length=255)
+	 * @Assert\NotBlank
 	 */
 	private $title;
 
